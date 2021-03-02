cdb4a89d94ee5afc89c796b6bb9d7b21c43f0f46
Mohamed Elsamny <mohamedsamny@gmail.com>
2021-03-02 15:08:42 -0500

    init react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+
+import React from 'react'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  }
+}
+
+export default App
